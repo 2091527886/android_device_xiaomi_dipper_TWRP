ebc60f2f850cfae0b935af238a3cce84d7723a6e
KyuoFoxHuyu <88403766@qq.com>
2022-01-04 23:21:45 +0800

    dipper: Bring up TWRP
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- twrp_dipper.mk
+++ twrp_dipper.mk
@@ -20,17 +20,17 @@ PRODUCT_RELEASE_NAME := dipper
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_dipper
+PRODUCT_NAME := twrp_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
